572902bb9bbf47dea973ac72c6690ad93c537d99
Charles Kuch <charlie.kuch@gmail.com>
2018-11-15 14:47:25 -0600

    Testing angle stuff by making everything move in a circle (it's a little magic but um well that's creative coding i guess)

--- paper_planes/src/flock.cpp
+++ paper_planes/src/flock.cpp
@@ -2,10 +2,8 @@
 
 Flock::Flock() {
 	light.setAmbientColor(ofColor(128.0f, 0, 0));
-	cone.set(.5f, 1.0f);
-	cone.rollDeg(90.0f);
-	//cone.tiltDeg(90.0f);
-	//cone.rotateDeg() // rotate cone be properly oriented when pointing
+	cone.set(.25f, 0.5f);
+	// rotate cone be properly oriented when pointing
 }
 
 void Flock::init(int n_planes) {
@@ -20,7 +18,9 @@ void Flock::init(int n_planes) {
 	ofVec3f velocity;
 	ofVec3f acceleration(0, 0, 0); //linear motion for now
 
+	float dTheta = TWO_PI / (float)n_planes;
 	for (int i = 0; i < n_planes; i++) {
+		/*
 		position.x = (ofRandom(1.0f) - 0.5f) * POSITION_DISPERSION;
 		position.y = (ofRandom(1.0f) - 0.5f) * POSITION_DISPERSION;
 		position.z = (ofRandom(1.0f) - 0.5f) * POSITION_DISPERSION;
@@ -28,6 +28,12 @@ void Flock::init(int n_planes) {
 		velocity.x = (ofRandom(1.0f) - 0.5f) * VELOCITY_DISPERSION;
 		velocity.y = (ofRandom(1.0f) - 0.5f) * VELOCITY_DISPERSION;
 		velocity.z = (ofRandom(1.0f) - 0.5f) * VELOCITY_DISPERSION;
+		*/
+
+		// doing circles as a test
+		float theta = (float)i * dTheta;
+		position = ofVec3f(10.0f * cos(theta), 10.0f * sin(theta), 0.0f);
+		velocity = ofVec3f(10.0f * -sin(theta), 10.0f * cos(theta), 0.0f);
 
 		paper_plane plane;
 		plane.position = position;
@@ -46,9 +52,18 @@ void Flock::customDraw() {
 	ofPushStyle();
 	light.enable();
 	light.setPosition(ofVec3f(0, 0, 0));
+
+	ofVec3f arrowTail, arrowHead;
 	for (unsigned int i = 0; i < planes.size(); i++) {
+		arrowTail = planes[i].position;
+		arrowHead = arrowTail + planes[i].velocity;
+		
+		ofDrawArrow(arrowTail, arrowHead, 0.0f);
+		//ofDrawSphere(planes[i].position, 0.5f);
 		cone.setPosition(planes[i].position);
-		//cone.lookAt(planes[i].position + planes[i].velocity);
+		cone.lookAt(planes[i].position + planes[i].velocity.normalize());
+		//cone.rollDeg(90.0f);
+		//cone.panDeg(90.0f);
 		cone.draw();
 	}
 
@@ -61,20 +76,23 @@ void Flock::update() {
 	// time differential used for updating vectors
 	float dt = ofGetLastFrameTime();
 
+	float dTheta = TWO_PI / (float)planes.size();
 	for (unsigned int i = 0; i < planes.size(); i++) {
 		// ok so basically numerically integrate
 		// v = dx / dt
 		// dx = v * dt
 		// x = x + dx
 		planes[i].position += planes[i].velocity * dt;
-		
+		//planes[i].position = ofVec3f(10.0f * cos(theta), 10.0f * sin(theta), 0.0f);
 		// for now, velocity is constant.
 		// in the future, acceleration will be non-zero
 		// the flocking rules will be called here to change accel
-		planes[i].velocity += planes[i].acceleration * dt;
+		//planes[i].velocity += planes[i].acceleration * dt;
+		float theta = (float)i * dTheta + (float)ofGetFrameNum() / PI * 100.0f;
+		planes[i].velocity = ofVec3f(10.0f * -sin(theta), 10.0f * cos(theta), 0.0f);
 
 		if (planes[i].position.lengthSquared() >= MAX_RADIUS * MAX_RADIUS) {
-			planes[i].velocity = -planes[i].velocity;
+			planes[i].velocity = -(planes[i].velocity);
 		}
 	}
 }
